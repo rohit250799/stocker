8783c03c71ca61c03e9ad2f502d2141c1e2acc01
Rohit Sarkar <rohitsarkar034@gmail.com>
2025-02-23 17:29:31 +0530

    add: connectDB function

--- main.go
+++ main.go
@@ -6,8 +6,56 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
+
+    "database/sql"
+    _ "github.com/lib/pq"
 )
 
+func connectDB() (*sql.DB, error) {
+    db_host := os.Getenv("DB_HOST");
+    db_port := os.Getenv("DB_PORT");
+    db_user := os.Getenv("POSTGRES_USER")
+    db_password := os.Getenv("POSTGRES_PASSWORD")
+    db_name := os.Getenv("POSTGRES_DB")
+
+    //for PG-admin
+    type User struct {
+        Name string `db:"username"`
+        Email string `db:"email"`
+    }
+
+    connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+        db_host, db_port, db_user, db_password, db_name);
+    db, err := sql.Open("postgres", connStr);
+    if err != nil {
+        log.Fatalln(err);
+    }
+
+    defer db.Close();
+
+    //Testing connection to db
+    if err := db.Ping(); err != nil {
+        log.Fatal(err)
+    } else {
+        log.Println("Successfully connected to db");
+    }
+
+    //performing operations in db
+    place := User{};
+    rows, err := db.Query("SELECT username, email FROM USERS");
+    if err != nil {
+        return nil, err;
+    }
+
+    defer rows.Close();
+
+    for rows.Next() {
+        
+    }
+
+
+    return db, nil;
+}
 
 
 func main() {
@@ -18,8 +66,6 @@ func main() {
         log.Fatalf("Error loading to .env file: %s", err)
     }
 
-    my_db_port := os.Getenv("DB_PORT") 
-
-    fmt.Println(my_db_port)
+    connectDB()
 }
 

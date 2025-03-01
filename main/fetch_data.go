9874d94d7dff3afc17ebc450331e1bdb389f0d42
Rohit Sarkar <rohitsarkar034@gmail.com>
2025-03-01 12:36:22 +0530

    add: fetch_data file to make requests for data

---
+++ main/fetch_data.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+func check_market_open_status() bool {
+	api_key := os.Getenv("apikey")
+	url := "https://www.alphavantage.co/query?function=MARKET_STATUS"
+	response, err := http.Get("https://www.alphavantage.co/query?function=MARKET_STATUS&apikey=")
+
+	if err != nil {
+		fmt.Print(err.Error())
+		return false
+	}
+
+	responseData, err := io.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(responseData))
+	return true
+}
+
+func get_market_data() {
+	if check_market_open_status() {
+		//response, err := http.Get("")
+		fmt.Println("The market is open right now.")
+		os.Exit(0)
+
+	} else {
+		fmt.Println("The market is not open right now.")
+		os.Exit(1)
+	}
+}

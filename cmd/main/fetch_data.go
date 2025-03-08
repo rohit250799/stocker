f66d8e0fe6cf6a252e97e32b701a5210a7185b9e
Rohit Sarkar <rohitsarkar034@gmail.com>
2025-03-08 08:24:13 +0530

    update: check_all_market_open_status function

--- cmd/main/fetch_data.go
+++ cmd/main/fetch_data.go
@@ -5,32 +5,37 @@ import (
 	"io"
 	"log"
 	"net/http"
-	//"net/url"
 	"os"
-	//"github.com/joho/godotenv"
 )
 
-func check_market_open_status() bool {
-	//api_key := os.Getenv("apikey")
-	// url := "https://www.alphavantage.co/query?function=MARKET_STATUS&apikey="
-	response, err := http.Get("https://www.alphavantage.co/query?function=MARKET_STATUS&apikey=test_key")
 
-	if err != nil {
-		fmt.Print(err.Error())
-		return false
+func check_all_market_open_status() bool {
+	api_key := os.Getenv("apikey")
+	if api_key == ""{
+		log.Fatal("Api key not found in .env file")
 	}
+	
+	baseUrl := "https://www.alphavantage.co/query"
+	queryParams := "?function=MARKET_STATUS&apikey=" + api_key
+	fullUrl := baseUrl + queryParams
+
+	response, err := http.Get(fullUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
 
 	responseData, err := io.ReadAll(response.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	fmt.Println(string(responseData))
 	return true
 }
 
 func get_market_data() {
-	if check_market_open_status() {
-		//response, err := http.Get("")
+	if check_all_market_open_status() {
 		fmt.Println("The market is open right now.")
 		os.Exit(0)
 

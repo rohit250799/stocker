f8dd0e1d45fdabafa53ac09911205a389ce3aaa6
Rohit Sarkar <rohitsarkar034@gmail.com>
2025-03-14 20:21:24 +0530

    add: insert company overview data function and remove: perform db operations function

--- pkg/db/schema.go
+++ pkg/db/schema.go
@@ -3,6 +3,7 @@ package db
 import (
 	"database/sql"
 	"fmt"
+	"log"
 )
 
 //for PG-admin
@@ -66,6 +67,12 @@ type Company_overview struct {
 	ExDividendDate             string  `json:"ExDividendDate"`
 }
 
+type Demo_Company_overview struct {
+	Symbol                     string  `json:"Symbol"`
+	AssetType                  string  `json:"AssetType"`
+	Name                       string  `json:"Name"`
+}
+
 func CreateTable(db *sql.DB) error {
 	fmt.Println("Enter the name of the table: ")
 
@@ -96,35 +103,21 @@ func CreateTable(db *sql.DB) error {
 	return nil
 }
 
-func perform_db_operations(user User) ([]User, error){
+func Insert_Company_Overview_data(comp Company_overview) (int64, error) {
+	db_connection_pointer, error := ConnectDB()
+	if error != nil {
+		log.Fatal(error)
+	}
+	result, err := db_connection_pointer.Exec("INSERT INTO demo_company_overview (symbol, assettype, stockname) VALUES ($1, $2, $3)", comp.Symbol, comp.AssetType, comp.Name)
 
-	db, err := ConnectDB();
 	if err != nil {
-		fmt.Println("There were some internal problems")
-		//return err
+		return 0, fmt.Errorf("addCompany_details: %v", err)
 	}
-	//performing operations in db
-	// db_custom_users := User{};
-	rows, err := db.Query("SELECT username, email FROM USERS");
+
+	id, err := result.LastInsertId()
 	if err != nil {
-		return nil, err;
+		return 0, fmt.Errorf("addCompany_details: %v", err)
 	}
-
-	defer rows.Close();
-
-	var db_custom_users []User;
-
-	for rows.Next() {
-		var database_user User;
-		if err := rows.Scan(&database_user.Name, &database_user.Email); err != nil {
-			return db_custom_users, err; 
-		}
-		db_custom_users = append(db_custom_users, database_user)
-	}
-
-	if err = rows.Err(); err != nil {
-		return db_custom_users, err
-	}
-
-	return db_custom_users, nil
+	return id, nil
+	
 }

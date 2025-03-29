1e2715b8a3e114d549f8f7fe71fd07fc414446cb
Rohit Sarkar <rohitsarkar034@gmail.com>
2025-03-29 23:13:07 +0530

    add: function call to insert weekly series data to db

--- cmd/stocker/main.go
+++ cmd/stocker/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	//"database/sql"
 	"fmt"
 	"log"
 
@@ -27,13 +26,14 @@ func main() {
         log.Fatalf("Error loading to .env file: %s", err)
     }
 
-    //connect to db and create a new table
+    //connect to db 
 
     db_connect, err := db.ConnectDB();
     if err != nil {
         fmt.Println("Error connecting to database:", err)
         return
     }
+
     defer db_connect.Close()
 
     //get_market_data
@@ -42,13 +42,19 @@ func main() {
     fmt.Println("Enter the company symbol to use: ") 
     fmt.Scan(&company_symbol)
 
-    //Inserting data from api response to db
+    // //Inserting company overview to db
 
     company, err := db.Insert_Company_Overview_data(company_symbol)
     if err != nil {
         log.Fatal(err)
     }
     fmt.Printf("The entered symbol is: %v", company)
-      
+
+    //Insert weekly series data to db
+    company_weekly_data, err := db.Insert_Time_Series_Weekly_data(company_symbol)
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Printf("The weekly timeseries is entered on: %v", company_weekly_data)  
 }
 

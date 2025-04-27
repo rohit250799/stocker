47f800f739f18f614175026baef8363c4c605178
Rohit Sarkar <rohitsarkar034@gmail.com>
2025-04-27 20:08:11 +0530

    add:makeHandler, renderTemplate and saveHandler functions, modify: editHandler and viewHandler functions

--- cmd/stocker/handleTemplates.go
+++ cmd/stocker/handleTemplates.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	// "html/template"
-	// "net/http"
-	// "path/filepath"
-	"go-stocker/templates"
 	"fmt"
 	"html/template"
 	"net/http"
 	"os"
-	//"context"
+	"regexp"
 )
 
 type Page struct {
@@ -17,13 +13,19 @@ type Page struct {
 	Body  []byte
 }
 
+var templates = template.Must(template.ParseGlob(
+	"templates/layouts/*.html",
+))
+
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
 // handling persistent storage
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
 	return os.WriteFile(filename, p.Body, 0600)
 }
 
-func LoadPage(title string) (*Page, error) {
+func loadPage(title string) (*Page, error) {
 	filename := title + ".txt"
 	body, err := os.ReadFile(filename)
 	if err != nil {
@@ -36,78 +38,50 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s", r.URL.Path[1:])
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
-	p, _ := LoadPage(title)
-	t, _ := template.ParseFiles("view.html")
-	t.Execute(w, p)
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		fn(w, r, m[2])
+	}
 }
 
-func editHandler(w http.ResponseWriter, r http.Request) {
-	title := r.URL.Path[len("/edit/"):]
-	p, err := LoadPage(title)
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	// path := "templates/layouts/" + tmpl + ".html"
+	err := templates.ExecuteTemplate(w, tmpl, p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+}
+
+func ViewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
+func EditHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
 	if err != nil {
 		p = &Page{Title: title}
 	}
-	t, _ := template.ParseFiles("edit.html")
-	t.Execute(w, p)
+	renderTemplate(w, "edit", p)
 }
 
-// func LoginButtonLandingPage() templ.Component {
-// 	//templates.RegisterButton("Register").Render(context.Background(), os.Stdout)
-// 	//return templates.LoginButton("Login")
-// 	return templ.ComponentGroup(
-// 		templates.LoginButton("Login"),
-// 		templates.RegisterButton("Register"),
-// 	)
-// }
-// // loadTemplates parses templates once and organizes them
-// func LoadTemplates() {
-// 	templates = make(map[string]*template.Template)
-
-// 	pages := []string{"home", "about"}
-
-// 	for _, page := range pages {
-// 		tmpl := template.Must(template.ParseFiles(
-// 			//filepath.Join("templates", "layouts", "base.html"),
-// 			filepath.Join("../../templates/layouts/base.html"),
-// 			filepath.Join("../../templates/pages/home.html"),
-
-// 			//filepath.Join("templates", "pages", page+".html"),
-// 		))
-// 		templates[page] = tmpl
-// 	}
-// }
-
-// func HomeHandler(w http.ResponseWriter, r *http.Request) {
-// 	data := struct {
-// 		Title    string
-// 		Username string
-// 	}{
-// 		Title:    "Home",
-// 		Username: "John Doe",
-// 	}
-// 	RenderTemplate(w, "home", data)
-// }
-
-// func AboutHandler(w http.ResponseWriter, r *http.Request) {
-// 	data := struct {
-// 		Title string
-// 	}{
-// 		Title: "About",
-// 	}
-// 	RenderTemplate(w, "about", data)
-// }
-
-// // renderTemplate renders the chosen page
-// func RenderTemplate(w http.ResponseWriter, name string, data interface{}) {
-// 	tmpl, ok := templates[name]
-// 	if !ok {
-// 		http.Error(w, "The page does not exist.", http.StatusNotFound)
-// 		return
-// 	}
-// 	err := tmpl.Execute(w, data)
-// 	if err != nil {
-// 		http.Error(w, "Template execution error.", http.StatusInternalServerError)
-// 	}
-// }
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
